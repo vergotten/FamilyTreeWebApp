e58fd4b345ca325e562d2690412f209f5220ddc3
vergotten <vergotten@gmail.com>
2023-10-30 16:33:03 +0300

    Database path fixed.

--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+import os
+
+
 class Config(object):
     SECRET_KEY = 'secret123321qweipoasm'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:////mnt/e/Python_Flask/FamilyTreeWebApp/db.db'
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.db')
